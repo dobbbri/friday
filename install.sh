1c626f7099c8cef88defd8373233cd618d0c0156
Sergio Dobri <sergiodobri@gmail.com>
2020-05-21 16:55:48 -0300

    fix scrit

--- install.sh
+++ install.sh
@@ -1,32 +1,10 @@
 #!/bin/sh
+
+# to make execurable
+# chmod +x install.sh
+
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Greetings. Preparing to power up and begin diagnostics.$(tput sgr 0)"
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Backup up current files.$(tput sgr 0)"
-
-# Backup files that are provided by the Jarvis into a ~/$HOME-backup directory
-BACKUP_DIR=$HOME/backup
-
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-echo "$(tput setaf 2)JARVIS: Creating backup directory at $BACKUP_DIR.$(tput sgr 0)"
-
-# Create backup folder if it doesn't exist
-mkdir -p $BACKUP_DIR
-
-files=("$HOME/.config/nvim" "$HOME/.zshrc" "$HOME/.tmux.conf" "$HOME/.wgetrc" "$HOME/.gitconfig" "$HOME/.global_ignore" "$HOME/.global_ignore")
-for filename in "${files[@]}"; do
-    if [ ! -L $filename ]; then
-      echo "$(tput setaf 2)JARVIS: Backing up $filename.$(tput sgr 0)"
-      mv $filename $BACKUP_DIR 2>/dev/null
-    else
-      echo -e "$(tput setaf 3)JARVIS: $filename does not exist at this location or is a symlink.$(tput sgr 0)"
-    fi
-done
-
-echo "$(tput setaf 2)JARVIS: Backup completed.$(tput sgr 0)"
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Checking for Homebrew installation.$(tput sgr 0)"
@@ -40,29 +18,8 @@ else
   echo "$(tput setaf 3)FRIDAY: Installing Homebrew. Homebrew requires osx command lines tools, please download xcode first$(tput sgr 0)"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew analytics off
+  echo "$(tput setaf 2)FRIDAY: Homebrew installed.$(tput sgr 0)"
 fi
-
-echo "$(tput setaf 2)FRIDAY: Installing Homebrew system packages.$(tput sgr 0)"
-brew_packages=(
-  "git"
-  "node"
-#  "tmux"
-  "neovim"
-  "python3"
-  "zsh"
-  "ripgrep"
-  "fzf"
-  "z"
-  "autoconf"
-  "makedepend"
-  "mas"
-p)
-
-for i in "${brew_packages[@]}"
-do
-  brew install $i
-done
-
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Installing Homebrew Cask packages.$(tput sgr 0)"
@@ -72,29 +29,57 @@ tap_packages=(
   "homebrew/cask"
   "homebrew/cask-fonts"
   "homebrew/core"
-p)
+)
 
-for i in "${tap_packages[@]}"
+for package in "${tap_packages[@]}"
 do
-  brew tap $i
+  echo "$(tput setaf 2)FRIDAY: Brew Tapping $package.$(tput sgr 0)"
+  brew tap $package
 done
+echo "$(tput setaf 2)FRIDAY: Homebrew Tap installed.$(tput sgr 0)"
 
+echo "$(tput setaf 2)FRIDAY: Installing Homebrew System Packages.$(tput sgr 0)"
+brew_packages=(
+  "autoconf"
+  "fzf"
+  "git"
+  "makedepend"
+  "mas"
+  "neovim"
+  "node"
+  "python3"
+  "ripgrep"
+  "youtube-dl"
+#  "tmux"
+  "z"
+  "zsh"
+)
+
+for package in "${brew_packages[@]}"
+do
+  echo "$(tput setaf 2)FRIDAY: Installing $package.$(tput sgr 0)"
+  brew install $package
+done
+echo "$(tput setaf 2)FRIDAY: Homebrew System Package installed.$(tput sgr 0)"
+
+echo "$(tput setaf 2)FRIDAY: Installing Homebrew Cash Apps.$(tput sgr 0)"
 cask_packages=(
+  "cyberduck"
   "font-hack-nerd-font"
+  "google-chrome"
+  "iterm2"
   "sublime-text"
   "sublime-merge"
-  "google-chrome"
-  "vlc"
   "transmission"
-  "cyberduck"
-  "youtube-dl"
-  "iterm2"
-p)
+  "vlc"
+)
 
-for i in "${cask_packages[@]}"
+for package in "${cask_packages[@]}"
 do
-  brew cask install $i
+  echo "$(tput setaf 2)FRIDAY: Installing $package.$(tput sgr 0)"
+  brew cask install $package
 done
+echo "$(tput setaf 2)FRIDAY: Homebrew Cask Apps installed.$(tput sgr 0)"
 
 
 echo "---------------------------------------------------------"
@@ -110,12 +95,15 @@ pop_packages=(
   "https://pilotmoon.com/popclip/extensions/ext/Calculate.popclipextz"
   "https://pilotmoon.com/popclip/extensions/ext/OpenInChrome.popclipextz"
   "https://pilotmoon.com/popclip/extensions/ext/Todoist.popclipextz"
-p)
+)
 
-for i in "${pop_packages[@]}"
+for package in "${pop_packages[@]}"
 do
-  wget $i
+  echo "$(tput setaf 2)FRIDAY: Downloading $package.$(tput sgr 0)"
+  wget $package
 done
+echo "$(tput setaf 2)FRIDAY: Homebrew PopClip package downloaded.$(tput sgr 0)"
+
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Installing Mas packages.$(tput sgr 0)"
@@ -137,12 +125,15 @@ mas_packages=(
   "585829637"  #Todoist
   "1384080005" #Tweetbot
   "1147396723" #WhatsAppas_packages=(
-p)
+)
 
-for i in "${mas_packages[@]}"
+for package in "${mas_packages[@]}"
 do
-  mas install $i
+  echo "$(tput setaf 2)FRIDAY: Installing $package.$(tput sgr 0)"
+  mas install $package
 done
+echo "$(tput setaf 2)FRIDAY: Mas Packages installed.$(tput sgr 0)"
+
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Installing Python packages.$(tput sgr 0)"
@@ -150,12 +141,14 @@ echo "$(tput setaf 2)FRIDAY: Installing Python packages.$(tput sgr 0)"
 pip3_packages=(
   "neovim"
   "vim-vint"
-p)
+)
 
-for i in "${pip3_packages[@]}"
+for package in "${pip3_packages[@]}"
 do
-  pip3 install $i
+  echo "$(tput setaf 2)FRIDAY: Installing $package.$(tput sgr 0)"
+  pip3 install $package
 done
+echo "$(tput setaf 2)FRIDAY: Python Packages installed.$(tput sgr 0)"
 
 
 echo "---------------------------------------------------------"
@@ -164,19 +157,21 @@ echo "$(tput setaf 2)FRIDAY: Installing Node Global package$(tput sgr 0)"
 node_packages=(
   "neovim"
   "spaceship-prompt"
-  "bash-language-serve"
+  "bash-language-server"
   "eslint"
   "prettier"
   "vtop"
   "@vue/cli"
   "serve"
   "typescript"
-p)
+)
 
-for i in "${node_packages[@]}"
+for package in "${node_packages[@]}"
 do
-  npm install $i
+  echo "$(tput setaf 2)FRIDAY: Installing $package.$(tput sgr 0)"
+  npm i -g $package
 done
+echo "$(tput setaf 2)FRIDAY: Node Packages installed.$(tput sgr 0)"
 
 
 echo "---------------------------------------------------------"
@@ -184,9 +179,11 @@ echo "$(tput setaf 2)FRIDAY: Installing oh-my-zsh.$(tput sgr 0)"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "$(tput setaf 2)FRIDAY: oh-my-zsh installed.$(tput sgr 0)"
 else
   echo "$(tput setaf 2)FRIDAY: oh-my-zsh already installed.$(tput sgr 0)"
 fi
+
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Checking for Git installation.$(tput sgr 0)"
@@ -197,8 +194,47 @@ if ! [[ -f "$GIT_DIR" ]]; then
   exit 1
 fi
 
-echo "$(tput setaf 2)FRIDAY: Installing zsh-autosuggestions.$(tput sgr 0)"
+echo "$(tput setaf 2)FRIDAY: Installing Git Packages.$(tput sgr 0)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo "$(tput setaf 2)FRIDAY: Git Packages installed.$(tput sgr 0)"
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)FRIDAY: Backup up current dotfiles.$(tput sgr 0)"
+
+# Backup files that are provided by the Jarvis into a ~/$HOME-backup directory
+BACKUP_DIR=$HOME/backup
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Create backup folder if it doesn't exist
+if [ ! -d $BACKUP_DIR ]; then
+  echo "$(tput setaf 2)FRIDAY: Creating backup directory at $BACKUP_DIR.$(tput sgr 0)"
+  mkdir -p $BACKUP_DIR
+fi
+
+files=(
+  ".config/nvim"
+  ".gitconfig"
+  ".global_ignore"
+  ".global_ignore"
+  ".tmux.conf"
+  ".wgetrc"
+  ".zshrc"
+)
+
+for filename in "${files[@]}"; do
+    if [ -L $HOME/$filename ]; then
+      echo "$(tput setaf 2)FRIDAY: Backing up $filename.$(tput sgr 0)"
+      mv $HOME/$filename $BACKUP_DIR/$filename
+    else
+      echo "$(tput setaf 3)FRIDAY: $filename does not exist at $HOME.$(tput sgr 0)"
+    fi
+done
+
+echo "$(tput setaf 2)FRIDAY: Backup completed.$(tput sgr 0)"
 
 
 echo "---------------------------------------------------------"
@@ -210,26 +246,32 @@ ln -s $DOTFILES_DIR/git/gitignore_global $HOME/.gitignore_global
 ln -s $DOTFILES_DIR/git/global_ignore $HOME/.global_ignore
 ln -s $DOTFILES_DIR/zsh/wgetrc $HOME/.wgetrc
 ln -s $DOTFILES_DIR/tmux/tmux.conf $HOME/.tmux.conf
+echo "$(tput setaf 2)FRIDAY: Linked dotfiles.$(tput sgr 0)"
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing nvim config files.$(tput sgr 0)"
+echo "$(tput setaf 2)FRIDAY: Installing Neovim config files.$(tput sgr 0)"
+
 # Create config folder if it doesn't exist
 CONFIG_DIR=$HOME/.config
 if [ ! -d $CONFIG_DIR ]; then
     echo "Creating ~/.config"
-    mkdir -p $CONFIG_DIR
+    mkdir -p $CONFIG_DIR/nvim
 fi
 
 config=$DOTFILES_DIR/nvim
 target=$CONFIG_DIR/nvim
-if [ -e $target ]; then
-  echo "$(tput setaf 3)JARVIS: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
+if [ -e $target/snippets ]; then
+  echo "$(tput setaf 3)FRIDAY: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
 else
-  echo "$(tput setaf 2)JARVIS: Creating symlink for ${config}.$(tput sgr 0)"
-  ln -s $config $target
+  echo "$(tput setaf 2)FRIDAY: Creating symlink for ${config}.$(tput sgr 0)"
+  ln -s $config/coc-settings.json $target/coc-settings.json
+  ln -s $config/plugins.vim $target/plugins.vim
+  ln -s $config/init.vim $target/init.vim
+  ln -s $config/snippets $target/snippets
 fi
 
+curl -fLo $CONFIG_DIR/nvim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 nvim +UpdateRemotePlugins +qall
 
@@ -237,7 +279,8 @@ nvim +UpdateRemotePlugins +qall
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Installing Space vim-airline theme.$(tput sgr 0)"
 
-cp ~/.config/nvim/space.vim ~/.config/nvim/plugged/vim-airline-themes/autoload/airline/themes/space.vim
+cp $config/space.vim ~/.config/nvim/plugged/vim-airline-themes/autoload/airline/themes/space.vim
+echo "$(tput setaf 2)FRIDAY: Neovim configured.$(tput sgr 0)"
 
 #echo "---------------------------------------------------------"
 #echo "$(tput setaf 2)FRIDAY: Installing tmux plugin manager.$(tput sgr 0)"
@@ -248,11 +291,11 @@ cp ~/.config/nvim/space.vim ~/.config/nvim/plugged/vim-airline-themes/autoload/a
 #fi
 
 echo "---------------------------------------------------------"
+echo "$(tput setaf 2)FRIDAY: Setting Mac defaults.$(tput sgr 0)"
+
 echo "$(tput setaf 2)FRIDAY: Disable touristd.$(tput sgr 0)"
 defaults write com.apple.touristd seed-https://help.apple.com/osx/mac/10.13.6/whats-new -date "$(date)"
 
-
-echo "---------------------------------------------------------"
 echo "$(tput setaf 2)FRIDAY: Remove all icons from Dock.$(tput sgr 0)"
 defaults write com.apple.dock persistent-apps -array
 defaults write com.apple.dock autohide-delay -float 0
